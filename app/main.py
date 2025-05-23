d33f8aff9133e0327b164d9bcd1368cbe46cb266
Maximus Champion <maximuschampion2002@gmail.com>
2025-05-23 13:56:32 +0300

    create logic to weather api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = "3213a637c33d45b4a5794241252305"
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Lviv&lang=uk"
+    response = requests.get(url)
+    data = response.json()
+
+    temperature = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    print(f"Погода у Львові: {temperature}°C, {condition}")
 
 
 if __name__ == "__main__":
