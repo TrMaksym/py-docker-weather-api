8df752c1671f56548723511f0085a43a253e5e7a
Maximus Champion <maximuschampion2002@gmail.com>
2025-05-24 14:00:31 +0300

    Change City Lviv, on Paris

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
+import os
+
 import requests
 
+CITY = "PARIS"
+API_KEY = os.environ.get('API_KEY')
 
-def get_weather() -> None:
-    api_key = "3213a637c33d45b4a5794241252305"
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Lviv&lang=uk"
-    response = requests.get(url)
-    data = response.json()
-
-    temperature = data["current"]["temp_c"]
-    condition = data["current"]["condition"]["text"]
-    print(f"Погода у Львові: {temperature}°C, {condition}")
-
+def get_weather():
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}&aqi=no"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()  # перевіряє статус-код (200-299)
+        data = response.json()
+        # тут парсимо і виводимо погоду
+        print(f"Current temperature in {CITY}: {data['current']['temp_c']}°C")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
 
 if __name__ == "__main__":
     get_weather()
+
